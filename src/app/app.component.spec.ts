5da677a3721c8e13162f647dce91104713904e09
Elhadj Alpha Amadou Diallo <Dialloalphaamadou947@gmail.com>
2025-02-20 16:23:38 +0000

    premiere modification

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,6 +24,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, unc-auto-deploy');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Bonjour Universite nongo conakry');
   });
 });
